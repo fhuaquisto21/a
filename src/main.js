69dfca9e57fee6c3da372d7eac8873bd3761304c
Your Name <you@example.com>
2022-12-12 18:02:25 +0000

    8305 movies

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ const pages_series = 64;
 const get_pelis = async () => {
   let iter_pelis = 1;
   for (let i = 1; i < pages_pelis + 1; i++) {
-    const browser = await chromium.launch({ executablePath: '/bin/brave', headless: false });
+    const browser = await chromium.launch({ executablePath: '/bin/brave-browser', headless: false });
     const context = await browser.newContext();
     const page = await context.newPage();
     await page.goto(`${url_pelis}${i}`, { timeout: 1000000 });
@@ -58,20 +58,23 @@ const get_series = async () => {
 }
 
 const get_content_pelis = async () => {
-  for (let i = 1; i < 8306; i++) {
+  for (let i = 6929; i < 8306; i++) {
     const crude = fs.readFileSync(`./data/pelis/${i}.json`);
     const data = JSON.parse(crude);
 
-    const browser = await chromium.launch({ executablePath: '/bin/brave', headless: false });
+    const browser = await chromium.launch({ executablePath: '/bin/brave-browser', headless: false });
     const context = await browser.newContext();
     const page = await context.newPage();
     await page.goto(data.url, { timeout: 1000000 });
 
-    const info_time = (await (await page.$('p.meta')).textContent()).trim().split('  ·  ');
+    const info_time = await page.$('p.meta') ? (await (await page.$('p.meta')).textContent()).trim().split('  ·  ') : ['', ''];
     const info_data = await Promise.all(
       (
         await page.$$('.info-list li')).map(async a => {
           const text_general = (await a.textContent()).trim().split(': ');
+	  if (text_general.length < 2) {
+	    return [];
+	  }
           if (text_general[0] == 'Actores') {
             return text_general[1].split(', ');
           }
@@ -86,7 +89,7 @@ const get_content_pelis = async () => {
     const info_players = await page.$$('#player > ul > li');
     const players = [];
     for (const inf_player of info_players) {
-      const type = (await (await inf_player.$('img')).getAttribute('src')).split('/').at(-1).split('.')[0];
+      const type = await inf_player.$('img') ? (await (await inf_player.$('img')).getAttribute('src')).split('/').at(-1).split('.')[0] : 'yt';
       if (type === 'yt') {
         continue;
       }
@@ -104,14 +107,16 @@ const get_content_pelis = async () => {
         const aurl =  await a.getAttribute('href');
         const type = (await (await a.$('img')).getAttribute('src')).split('/').at(-1).split('.')[0];
         const bpage = await context.newPage();
-        await bpage.goto(aurl);
+        await bpage.goto(aurl, { timeout: 10000000 });
         const url = await bpage.url();
         await bpage.close();
         return { url, type };
       })
     );
-    console.log(downloads);
-    break;
+    const sdata = JSON.stringify({ url: data.url, title: data.title, duration: info_time[0], year: info_time[1], genres: info_data[0], actors: info_data[1], description: info_description, cover: info_cover, players, downloads }, null, 4);
+    fs.writeFileSync(`./full/pelis/${i}.json`, sdata);
+    console.log(i);
+    await browser.close();
   }
 }
 
